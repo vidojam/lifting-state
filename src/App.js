9f9d8455635a4e8bb184f3e6b3f7db9722437cb9
Jose Torres <vidojam@gmail.com>
2023-04-20 11:11:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState } from "react"
-import Signup from "./components/Signup"
-import Login from "./components/Login"
-import Dashboard from "./components/Dashboard"
+import { useState } from 'react';
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
@@ -10,12 +10,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Lifting Up State</h1>
-        {!user && <></><section> style={{border: "2px, solid red", width: "100%", padding: "16px" }}</section>
-        <Signup setUser={setUser}/>
-        <section> style={{border: "2px, solid yellow", width: "100%", padding: "16px" }}</section>
-        <Login setUser={setUser}></>}
-        {user && <section> style={{border: "2px, solid blue", width: "100%", padding: "16px" }}
-        <Dashboard user={user} setUser={setUser}/>
+        {!user && <><section style={{ border: '2px solid red', width: '100%', padding: '16px'}}>
+          <Signup setUser={setUser} />
+        </section>
+        <section style={{ border: '2px solid yellow', width: '100%', padding: '16px'}}>
+          <Login setUser={setUser} />
+        </section></>}
+        {user && <section style={{ border: '2px solid blue', width: '100%', padding: '16px'}}>
+          <Dashboard user={user} setUser={setUser} />
         </section>}
       </header>
     </div>
